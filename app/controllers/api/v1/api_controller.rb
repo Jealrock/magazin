486538facbf95a1873e475551640f5cb1ba1b03a
Oleg Marakhovsky <oleg.marakhovsky@gmail.com>
2019-05-19 22:26:16 +0300

    Initiated offer controller/concepts/serializers

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,92 @@
+class Api::V1::ApiController < ApplicationController
+  MAX_PER_PAGE = 300
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  rescue_from ActionController::ParameterMissing, with: :bad_request
+  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def destroy_session
+    request.session_options[:skip] = true
+  end
+
+  private
+
+  def result_index(result, serializer_options)
+    render_error(result) && return unless result.success?
+
+    collection = apply_scopes(result['model']).page(page).per(per_page)
+    render json: {
+      data: serialize_resource(collection, serializer_options),
+      meta: {
+        pagination: {
+          page: collection.current_page,
+          per_page: collection.limit_value,
+          pages: collection.total_pages,
+          results_count: collection.size,
+          total_count: collection.total_count
+        }
+      }
+    }, status: 200
+  end
+
+  def result_show_create(result, serializer_options)
+    render_error(result) && return unless result.success?
+
+    render json: {
+      data: serialize_resource(result['model'], serializer_options)
+    }, status: 200
+  end
+
+  def result_delete(result, *)
+    render_error(result) && return unless result.success?
+
+    render json: {}, status: 204
+  end
+
+  def render_error(failed_result)
+    response = Response.new
+    response.add_error_message failed_result['error']
+    response.details = failed_result['contract.default']&.errors&.messages || {}
+    render json: response, status: failed_result['error.status']
+  end
+
+  def per_page
+    current_per_page = params[:per_page] ? params[:per_page].to_i : MAX_PER_PAGE
+    raise "Max per page limit is #{MAX_PER_PAGE}" if current_per_page > MAX_PER_PAGE
+
+    current_per_page
+  end
+
+  def page
+    params[:page] || 1
+  end
+
+  def serialize_resource(resource, serializer_options)
+    if serializer_options
+      ActiveModelSerializers::SerializableResource.new(resource, serializer_options).serializable_hash
+    else
+      result['model']
+    end
+  end
+
+  def bad_request(message = 'Bad request')
+    render json: { errors: [message] }, status: :bad_request
+  end
+
+  def forbidden(message = 'Forbidden')
+    render json: { errors: [message] }, status: :forbidden
+  end
+
+  def not_authorized(message = 'Not authorized')
+    render json: { errors: [message] }, status: :unauthorized
+  end
+
+  def not_found(message = 'Not found')
+    render json: { errors: [message] }, status: :not_found
+  end
+
+  def unprocessable_entity(exception)
+    render json: { errors: exception.record.errors.full_messages },
+           status: :unprocessable_entity
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_210315) do
+ActiveRecord::Schema.define(version: 2019_05_19_190415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "offers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
