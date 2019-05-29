8b1bb1d1b2ddda58e8a1690d8280a25aad0cc1af
Oleg Marakhovsky <oleg.marakhovsky@gmail.com>
2019-05-29 13:02:09 +0300

    Attempt to fix trailblazer uninitialized constant issue

--- config/initializers/trailblazer.rb
+++
@@ -1,8 +0,0 @@
-require 'trailblazer/operation/model'
-require 'trailblazer/operation/guard'
-require 'trailblazer/operation/contract'
-require 'trailblazer/operation/validate'
-
-# for validation, we want the reform gem with Dry-validation as the engine.
-require 'reform'
-require 'reform/form/dry'
