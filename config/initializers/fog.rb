1e435af2161e301cd3057d1b8ef7dac67441169f
Oleg Marakhovsky <oleg.marakhovsky@gmail.com>
2019-06-11 13:17:41 +0300

    Fixed fog aws config

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -3,8 +3,10 @@ CarrierWave.configure do |config|
     :provider              => 'AWS',
     :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
     :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-    :region                => ENV['S3_REGION']
+    :region                => ENV['S3_REGION'],
+    :endpoint              => 'https://s3.amazonaws.com'
   }
   config.fog_directory =  ENV['S3_BUCKET']
   config.fog_public = false
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
 end
