7055c52ebcbd96f5e2d0faa86c52decebed7a663
Oleg Marakhovsky <oleg.marakhovsky@gmail.com>
2019-07-19 13:26:09 +0300

    Added capistrano

---
+++ config/deploy/production.rb
@@ -0,0 +1,11 @@
+server '139.59.143.213', port: 22, roles: [:web, :app, :db], primary: true
+set :ssh_options, {
+  verify_host_key: false,
+  forward_agent: true,
+  user: "rails",
+  keys: ['../../.ssh/magazin-digitalocean']
+}
+set :deploy_to, "/home/rails/#{fetch(:application)}"
+set :user, "rails"
+set :runner, "rails"
+set :branch, "master"
