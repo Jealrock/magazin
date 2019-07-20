625f5b27346a9e629147f4c31bc08bb4e2c67116
Oleg Marakhovsky <oleg.marakhovsky@gmail.com>
2019-07-21 00:30:01 +0300

    Added action cable config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://#{ENV['APPLICATION_DOMAIN']}/api/v1/cable"
+  config.action_cable.allowed_request_origins = ["https://#{ENV['APPLICATION_DOMAIN']}", "http://#{ENV['APPLICATION_DOMAIN']}"]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
