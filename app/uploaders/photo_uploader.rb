855cfa755ca3f862c0e932dd0843bf27ae426f56
Oleg Marakhovsky <oleg.marakhovsky@gmail.com>
2019-08-02 23:15:01 +0300

    Make thumb image bigger

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -7,7 +7,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   end
 
   version :thumb do
-    process resize_to_limit: [150, 150]
+    process resize_to_limit: [400, 400]
   end
 
   def asset_host
